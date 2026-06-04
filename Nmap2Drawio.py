c21278fec31a96c0080991a606653ab364b1ab71
UnholyPresence <96213314+UnholyPresence@users.noreply.github.com>
2026-06-04 16:00:21 -0700

    Update Nmap2Drawio.py
    Kept working, did a test main. Still _incredibly_ WIPpy

--- Nmap2Drawio.py
+++ Nmap2Drawio.py
@@ -9,9 +9,9 @@
 # and have this script automatically generate you a graphic network map
 # ready for you to toss into a report. There are other tools out there
 # that do the same thing, but I plan to have this script be a module in
-# a broader, customizable report-generation automaition that takes 
+# a broader, customizable report-generation automation that takes
 # general and expected artifacts that you might discover over the course
-# of an engagment, and turn it into (most of) your report for you.
+# of an engagement, and turn it into (most of) your report for you.
 #
 # _-^-_MOAR HACKING. LESS WRITING._-^-_
 #   |                               |
@@ -45,3 +45,31 @@ class NmapHost:
 class NmapScan:
 	source_file: str
 	hosts: list[NmapHost] = field(default_factory=list)
+
+#Test Main
+def main():
+	ssh_port = NmapPort(
+		number=22,
+		protocol="tcp",
+		state="open",
+		service="ssh",
+		product="OpenSSH",
+		version="22.2"
+	)
+	test_host = NmapHost(
+		ip="127.0.0.1",
+		status="up",
+		hostnames=["127.0.0.1"],
+		mac="00:00:00:00:00:00",
+		vendor="Dell",
+		ports=[ssh_port]
+	)
+	scan = NmapScan(
+		source_file="manual-test",
+		hosts=[test_host]
+	)
+	print(scan)
+
+if __name__ == "__main__":
+	main()
+
